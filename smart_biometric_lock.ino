ed99cb2b3f6577123a49de2d5889a85d6203bff4
silver232 <93175201+silver232@users.noreply.github.com>
2023-06-23 00:20:26 +0530

    Update smart_biometric_lock.ino

--- smart_biometric_lock.ino
+++ smart_biometric_lock.ino
@@ -9,10 +9,6 @@ const char* password = "mmea2327";            //WIFI PASSWORD
 /* data received from application */
 String  data = "";
 
-
-int Relay1 = D0;    //D6
-int Relay2 = D1;    //D0
-int Relay3 = D2;     //D2
 int Relay4 = D4;     //D1
 int BUZZER = D5;     //D1
 
@@ -28,15 +24,9 @@ String months[12]={"January", "February", "March", "April", "May", "June", "July
 void setup()
 {
   /* initialize motor control pins as output */
-  pinMode(Relay1, OUTPUT);
-  pinMode(Relay2, OUTPUT);
-  pinMode(Relay3, OUTPUT);
   pinMode(Relay4, OUTPUT);
   pinMode(BUZZER, OUTPUT);
 
-  digitalWrite(Relay1, HIGH);
-  digitalWrite(Relay2, HIGH);
-  digitalWrite(Relay3, HIGH);
   digitalWrite(Relay4, HIGH);
   digitalWrite(BUZZER, LOW);
 
@@ -55,70 +45,9 @@ void loop()
   if (!client) return;
   data = checkClient ();
   Serial.print(data);
-  /******** Run function according to incoming data from application *********/
+  /******** Run function according to incoming data from application *********
 
-
-  if (data == "Relay1ON")
-  {
-    digitalWrite(Relay1, LOW);
-         timeClient.update();
-  
-  time_t epochTime = timeClient.getEpochTime();
-  
-  String formattedTime = timeClient.getFormattedTime();
-  Serial.print("\nAccess Time: ");
-  Serial.println(formattedTime);   
-
-  String weekDay = weekDays[timeClient.getDay()];
-  Serial.print("Week Day: ");
-  Serial.println(weekDay);    
-
-  //Get a time structure
-  struct tm *ptm = gmtime ((time_t *)&epochTime); 
-
-  int monthDay = ptm->tm_mday;
-  Serial.print("Month day: ");
-  Serial.println(monthDay);
-
-  int currentMonth = ptm->tm_mon+1;
-  Serial.print("Month: ");
-  Serial.println(currentMonth);
-
-  String currentMonthName = months[currentMonth-1];
-  Serial.print("Month name: ");
-  Serial.println(currentMonthName);
-
-  int currentYear = ptm->tm_year+1900;
-  Serial.print("Year: ");
-  Serial.println(currentYear);
-  }
-
-  else if (data == "Relay1OFF")
-  {
-    digitalWrite(Relay1, HIGH);
-  }
-
-  else if (data == "Relay2ON")
-  {
-    digitalWrite(Relay2, LOW);
-  }
-
-  else if (data == "Relay2OFF")
-  {
-    digitalWrite(Relay2, HIGH);
-  }
-
-  else if (data == "Relay3ON")
-  {
-    digitalWrite(Relay3, LOW);
-  }
-
-  else if (data == "Relay3OFF")
-  {
-    digitalWrite(Relay3, HIGH);
-  }
-
-  else if (data == "RELAY4SUCCESS")
+  if (data == "RELAY4SUCCESS")
   {
     digitalWrite(Relay4,LOW);
     digitalWrite(BUZZER, HIGH);
@@ -158,7 +87,7 @@ void loop()
   }
 
 }
-
+//wifi connection establishing
 void connectWiFi()
 {
   Serial.println("Connecting to WIFI");
